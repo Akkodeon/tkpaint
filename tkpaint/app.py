d106f63d55cc601c72931d42f560bbd605da0ce9
kaparoo <kaparoo2001@gmail.com>
2022-08-11 02:30:43 +0900

    ✨ Make `TkPaint` to use `Window`

--- tkpaint/app.py
+++ tkpaint/app.py
@@ -4,12 +4,13 @@ __all__ = ["TkPaint"]
 
 import tkinter as tk
 
+from tkpaint.gui import Window
+
 
 class TkPaint(tk.Tk):
     def __init__(self):
         super(TkPaint, self).__init__()
-        self.title("TkPaint")
-        self.geometry("640x480+50+50")
+        self.window = Window(app=self)
 
 
 if __name__ == "__main__":
